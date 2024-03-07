7a012f10e71dea4443058585ea474031aa3f1e6b
jeromeXL <jerome.chiu99@gmail.com>
2024-03-07 22:09:58 +1100

    added helper functions

--- src/backend/router/admin.py
+++ src/backend/router/admin.py
@@ -20,18 +20,26 @@ async def createUser(user: User, adminUser = Depends(admin_user())) -> User.id:
     newUser.insert()
     return newUser.id
 
-# Update Password use DTO (data transfer object)
-class UpdatePasswordRequest(BaseModel):
+# Update User (Previously Update Password)
+class UpdateRequest(BaseModel):
+    newUsername: str
     newPassword: str
+    newRole: UserRole
     
 @router.patch("/update/{id}")
-async def updatePassword(id: str, newPasswordRequest: UpdatePasswordRequest, adminUser = Depends(admin_user())) -> bool:
+async def updateUser(id: str, updateRequest: UpdateRequest, adminUser = Depends(admin_user())) -> bool:
     if adminUser:
         user = await User.find_one(User.id == id)
-        await user.set({User.password: newPasswordRequest.newPassword})
+        await user.set(updateRequest)
 
 # Delete User
 @router.delete("/delete/{id}")
 async def deleteUser(id: str) -> bool:
     user = await User.find_one(User.id == id)
     await user.delete()
+
+# Fetch all users (Helper)
+#@router.get("/all")
+#async def allUsers():
+#    users = await User.find_all()
+#    return users
