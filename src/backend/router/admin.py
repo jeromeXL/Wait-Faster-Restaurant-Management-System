c8dfc64df349df6d7063c86a894bf41d6aace15b
unknown <z5309959@ad.unsw.edu>
2024-03-10 16:42:56 +1100

    Added HTTP error names

--- src/backend/router/admin.py
+++ src/backend/router/admin.py
@@ -10,7 +10,7 @@ router = APIRouter()
 @router.get("/users")
 async def getUsers(adminUser = Depends(admin_user)) -> list:
     if not adminUser:
-        raise HTTPException(status_code=401, detail="Only admins can get users info")
+        raise HTTPException(status_code=403, detail="403 Forbidden: Only admins can get users info")
     users = await User.find_all().to_list() # Users.find_all() returns a queryable. Then calling .to_list() will transform that into a list. 
     return users
 
@@ -18,7 +18,7 @@ async def getUsers(adminUser = Depends(admin_user)) -> list:
 @router.get("/user/{username}")
 async def getUser(username: str, adminUser = Depends(admin_user)) -> User:
     if not adminUser:
-        raise HTTPException(status_code=401, detail="Only admins can get user info")
+        raise HTTPException(status_code=403, detail="403 Forbidden: Only admins can get user info")
     user = await User.find_one(User.username == username)
     return user
 
@@ -26,12 +26,12 @@ async def getUser(username: str, adminUser = Depends(admin_user)) -> User:
 @router.post("/user/create")
 async def createUser(newUser: User, adminUser = Depends(admin_user)) -> User:
     if not adminUser:
-        raise HTTPException(status_code=401, detail="Only admins can create users")
+        raise HTTPException(status_code=403, detail="403 Forbidden: Only admins can create users")
     if not matchesTablePattern(newUser.username) and newUser.role == UserRole.CUSTOMER_TABLET:
-        raise HTTPException(status_code=405, detail="Table names must be in format: 'Table<Number>'")
+        raise HTTPException(status_code=405, detail="422 Unprocessable Entity: Table names must be in format 'Table<Number>'")
     newUsernameTaken = await User.find_by_username(newUser.username)
     if newUsernameTaken:
-        raise HTTPException(status_code=409, detail="New username already exists. Duplicate usernames not allowed")
+        raise HTTPException(status_code=409, detail="409 Conflict: New username already exists. Duplicate usernames not allowed")
     user = User(username=newUser.username, password=hash_password(newUser.password), role=newUser.role)
     await user.create() # On an instance, call create.
     return user
@@ -40,16 +40,16 @@ async def createUser(newUser: User, adminUser = Depends(admin_user)) -> User:
 @router.put("/user/update/{username}")
 async def updateUser(username: str, newUserInfo: User, adminUser = Depends(admin_user)) -> User:
     if not adminUser:
-        raise HTTPException(status_code=401, detail="Only admins can update users")
+        raise HTTPException(status_code=401, detail="403 Forbidden: Only admins can update users")
     if not matchesTablePattern(newUserInfo.username) and newUserInfo.role == UserRole.CUSTOMER_TABLET:
-        raise HTTPException(status_code=405, detail="Table names must be in format: 'Table<Number>'")
+        raise HTTPException(status_code=405, detail="422 Unprocessable Entity: Table names must be in format 'Table<Number>'")
     #user = await User.find_one(User.id == user_id) # cant find by id for some reason
     user = await User.find_by_username(username)
     if not user:
-        raise HTTPException(status_code=404, detail="User does not exist")
+        raise HTTPException(status_code=404, detail="404 Not Found: User does not exist")
     newUsernameTaken = await User.find_by_username(newUserInfo.username)
     if newUsernameTaken:
-        raise HTTPException(status_code=409, detail="New username already exists. Duplicate usernames not allowed")
+        raise HTTPException(status_code=409, detail="409 Conflict: New username already exists. Duplicate usernames not allowed")
     user.username = newUserInfo.username
     user.password = hash_password(newUserInfo.password)
     user.role = newUserInfo.role
@@ -61,10 +61,10 @@ async def updateUser(username: str, newUserInfo: User, adminUser = Depends(admin
 @router.delete("/user/delete/{username}")
 async def delete_user(username: str, adminUser = Depends(admin_user)) -> Response:
     if not adminUser:
-        raise HTTPException(status_code=401, detail="Only admins can delete users")
+        raise HTTPException(status_code=401, detail="403 Forbidden: Only admins can delete users")
     #user = await User.find_one(User.id == user_id) cant find by id for some reason
     user = await User.find_one(User.username == username)
     if not user:
-        raise HTTPException(status_code=404, detail="User does not exist")
+        raise HTTPException(status_code=404, detail="404 Not Found: User does not exist")
     await user.delete()
     return Response(status_code=200)
