ce0cff66ffddb9666f4627201229b5d389fbb238
z5310374@ad.unsw.edu.au <z5310374@ad.unsw.edu.au>
2024-03-12 16:59:39 +1100

    menu router stubs

--- src/backend/router/menu.py
+++ src/backend/router/menu.py
@@ -1,32 +1,24 @@
 from datetime import timedelta
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException, Security
 from pydantic import BaseModel
 from utils.user_authentication import admin_user
 from models.auth import AccessToken, RefreshToken
 from models.user import User
+# from models.menu import Category
 from utils.password import hash_password
 from jwt import access_security, refresh_security
-from fastapi_jwt import JwtAuthorizationCredentials
-from models.MenuObject import Menu
+# from fastapi_jwt import JwtAuthorizationCredentials
 
-router = APIRouter(prefix="/Menu", tags=["Menu"])
+router = APIRouter(prefix="/menu", tags=["Menu"])
 
 @router.get("")
 async def getMenu():
-    # return await Menu.find_all()
-    menu = Menu.find_all()
-    if not menu:
-        # impossible? fatal error?
-        raise HTTPException(status_code=404, detail="Menu not found")
-    return menu
+    # form the menu based on the categories listed in the menu.json file
+    # make sure each category exists in database
 
-@router.put("")
-async def updateMenu(changedMenu: Menu):
-    # return await Menu.find_all()
-    validated_menu = Menu.validate(changedMenu.model_dump())
-    menu = Menu.find_all()
-    if not menu:
-        # impossible? fatal error?
-        raise HTTPException(status_code=404, detail="Menu not found")
-    await menu.update(**changedMenu.model_dump(exclude_unset=True))
-    return menu
+@router.put("/reorder")
+async def reorderMenu(changedMenu: List[str]):
+    # token required, use auth stuff
+    # validate changedMenu has valid categories
+    # replace menu.json's item with jsonified menu
