aca5c13270525db593f6251ec49b5ceedc838a5a
unknown <z5309959@ad.unsw.edu>
2024-03-12 23:43:20 +1100

    Removed Uneccessary code

--- src/backend/utils/password.py
+++ src/backend/utils/password.py
@@ -3,7 +3,3 @@ import bcrypt
 def hash_password(password: str) -> str:
     """Return a salted password hash."""
     return bcrypt.hashpw(password.encode(), b"$2b$12$WjTCIJ/eNVCUKPO58sFpV.").decode() ## TEMPORARY SALT, PLEASE UPDATE WITH .env CONFIG
-
-def hashPassword(password: str) -> str:
-    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-    return hashed.decode()  
--- src/backend/utils/regex.py
+++ src/backend/utils/regex.py
@@ -4,6 +4,3 @@ tablePattern = r'^Table\d+$'
 
 def matchesTablePattern(username: str) -> bool:
     return re.match(tablePattern, username) is not None
-
-if __name__ == "__main__":
-    print(matchesTablePattern("TableOne"))
