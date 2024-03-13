75ff2fce4b2763c1cba7c1046c9ab41fcf2244b8
z5310374@ad.unsw.edu.au <z5310374@ad.unsw.edu.au>
2024-03-13 21:35:26 +1100

    category implementation passes all tests

--- src/backend/router/category.py
+++ src/backend/router/category.py
@@ -8,6 +8,7 @@ from models.user import User
 from models.menu import Category
 from utils.password import hash_password
 from jwt import access_security, refresh_security
+from bson.objectid import ObjectId
 # from fastapi_jwt import JwtAuthorizationCredentials
 
 router = APIRouter(prefix="/category", tags=["Menu"])
@@ -23,68 +24,72 @@ class CategoryResponse(BaseModel):
     menuItem: List[str]
     index: int
 
+class CategoryUpdate(BaseModel):
+    id: str
+    name: str
+    menuItem: List[str]
+    index: int = -1
+
 @router.post("/new", response_model=CategoryResponse)
-async def createCategory( newCategory: CategoryCreate):
+async def createCategory( newCategory: CategoryCreate, manager = Depends(admin_user)):
     # create a new category
     vCategory = Category.model_validate(newCategory.model_dump())
     if not vCategory.name.strip():
         raise HTTPException(status_code=400, detail="Category name cannot be empty")
     # find if it exists
-    category = await Category.find_one(name = vCategory.name)
+    category = await Category.find_one(Category.name == vCategory.name)
     if category:
         raise HTTPException(status_code=400, detail="Category name cannot be duplicated")
     newCategory = Category(**newCategory.model_dump())
     await newCategory.create()
     return CategoryResponse(**newCategory.model_dump())
 
-@router.put("/{categoryId}")
-async def updateCategory(categoryId: str, updatedCategory: Category):
-    vUpdateCategory = Category.model_validate(updatedCategory.model_dump())
-    category = Category.get_by_id(categoryId)
-    # Validation check
-    if not category:
-        raise HTTPException(status_code=404, detail="Category not found")
-    if not vUpdateCategory.name.strip():
-        raise HTTPException(status_code=400, detail="Category name cannot be empty")
-    await category.update(**updatedCategory.model_dump())
-
-@router.delete("/one/{categoryId}")
-async def deleteCategory(categoryId: str):
+@router.put("/{categoryId}", response_model=CategoryResponse)
+async def updateCategory(categoryId: str, updatedCategory: CategoryCreate, manager = Depends(admin_user)):
+    # vUpdateCategory = Category.model_validate(updatedCategory.model_dump())
     # category = Category.get_by_id(categoryId)
-    category = Category.find_one(Category.id==categoryId)
-    print("delete category with id: {categoryId}")
-    print(category.id)
-    if not category:
+    # category = Category.find_one(Category.id == categoryId)
+    try:
+        category = await Category.get(categoryId)
+    except:
         raise HTTPException(status_code=404, detail="Category not found")
-    # await category.delete()
+
+    if not updatedCategory.name.strip():
+        raise HTTPException(status_code=400, detail="Category name cannot be empty")
+    category.name = updatedCategory.name
+    category.menuItem = updatedCategory.menuItem
+    category.index = updatedCategory.index
+
+    return CategoryResponse(**category.model_dump())
+
+@router.delete("/{categoryId}")
+async def deleteCategory(categoryId: str, manager = Depends(admin_user)):
+    # category = Category.get_by_id(categoryId)
+    # category = Category.find_one(Category.id==categoryId)
+    # category = Category.find_one({"_id": categoryId})
+    try:
+        category = await Category.get(categoryId)
+        await category.delete()
+    except:
+        raise HTTPException(status_code=404, detail="Category not found")
+    print("deleted category with id: {categoryId}")
     # remove from menu.json also if it is there
     return {"message": "Category deleted successfully"}
 
 @router.put("/reorder")
-async def reorderMenu(changedMenu: List[str]):
+async def reorderMenu(changedMenu: List[str], manager = Depends(admin_user)):
 # for each category name in changedMenu, assign the index that
 # the category appears in in changedMenu: 0,1,2... etc
 #   throw when non-existent category is in changedMenu
     return {"message": "Reorder"}
 
-# debug / helper routes
-@router.get("/count")
-async def createCategory():
-    # create a new category
-    categorycount = await Category.count()
-    print(f"There are {categorycount} categories ")
-    return categorycount
+# debug helpers
 
 @router.get("/all")
 async def createCategory():
     async for result in Category.find():
     # async for result in Category.find().project(CategoryResponse).to_list():
         print(result)
-
-@router.delete("/all}")
-async def deleteCategory():
-    await Category.delete()
-    # async for result in Category.find():
-    #     print(result)
-    #     # await result.delete()
-    return {"message": "All Category deleted successfully"}
+    categorycount = await Category.count()
+    print(f"There are {categorycount} categories ")
+    return {"count" : categorycount}
